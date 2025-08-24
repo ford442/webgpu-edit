b2c1d041c2c55e6a36f635c4110f52c2e9cf1161
Noah Cohn <noahc42@gmail.com>
2025-08-23 19:14:46 -0700

    Update webgpu_compute_js_mod.cpp

--- src/vanilla/webgpu_compute_js_mod.cpp
+++ src/vanilla/webgpu_compute_js_mod.cpp
@@ -497,10 +497,10 @@ let fileStream=FS.open('/video/frame.gl','w');
 
 function canvasStartSize(){
 const media_mode = document.querySelector('#media').value;
-if(media_mode=='img'){
+if(media_mode==='img'){
 const vvic=document.querySelector('#ivi');
 }
-if(media_mode=='vid'){
+if(media_mode==='vid'){
 const vvic=document.querySelector('#mvi');
 }
 const srsiz=document.querySelector('#srsiz').innerHTML;
@@ -2002,6 +2002,7 @@ setTimeout(function(){
 document.querySelector('#di').click();
 },250);
 });
+
 
 
 
